df84eed852e8f9121b5b0d093cdb7f16ee3325fc
Kenneth <kenneth@kennt.dk>
2021-09-06 13:57:40 +0200

    started the Run pace task

--- Assignment_DE.2.0/js/TrackRun.js
+++ Assignment_DE.2.0/js/TrackRun.js
@@ -3,13 +3,32 @@
 const $ = function (foo) {
     return document.getElementById(foo);
 };
-const catcher = function (e) {
-    let t1 = $('it1').value;
-    $('res').innerHTML = t1 - 0;
+const convert = function (e) {
+    let km = $('km').value;
+    let hour = $('hour').value;
+    let min = $('minute').value;
+    let sec = $('second').value;
+    // convert running time to seconds
+    let totTime = sec+(min*60)+(hour*3600);
+    console.log(km/totTime);
+    // calculate total time pr. km
+    let pace = ((km/totTime)/60).toFixed(2);
+
+    
+/*
+    let arr = s.split(':');
+    let h = arr[0];
+    let m = arr[1];
+    let s = arr[2];
+*/  
+    $('hours').innerHTML = pace;
+    /*$('minutes').innerHTML = m;
+    $('seconds').innerHTML = s;
+    */
     e.preventDefault();
     return false;
 };
 const init = function () {
-    $('formalisme').addEventListener('submit', catcher);
+    $('t2tpkm').addEventListener('submit', convert);
 };
 window.addEventListener('load', init);
