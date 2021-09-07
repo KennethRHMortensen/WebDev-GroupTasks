c97a430c58b35c0e224439844dfc233928812d28
Kenneth <kenneth@kennt.dk>
2021-09-07 14:03:36 +0200

    added tasks to nav

--- Assignment_DE.2.0/js/TrackRun.js
+++ Assignment_DE.2.0/js/TrackRun.js
@@ -1,19 +1,48 @@
 'use strict'
 
+// create $ shortkey for document.getElementById
 const $ = function (foo) {
     return document.getElementById(foo);
 };
-const convert = function (e) {
-    let km = $('km').value;
-    let hour = $('hour').value;
-    let min = $('minute').value;
-    let sec = $('second').value;
-    // convert running time to seconds
-    let totTime = sec+(min*60)+(hour*3600);
-    console.log(km/totTime);
-    // calculate total time pr. km
-    let pace = ((km/totTime)/60).toFixed(2);
 
+let km = $('km').value;
+let hour = $('hour').value;
+let min = $('minute').value;
+let sec = $('second').value;
+
+// converting function to find running pace pr. km
+const convert = function (e) {
+    
+    // convert total running time to seconds
+    let totalTime = sec+(min*60)+(hour*3600);
+    console.log(km/totalTime);
+
+    // convert total time in seconds to total time in seconds pr. km 
+    let paceSec = km/totTime;
+    // convert time pr. km. from seconds to hours, then substract the remaining minutes and seconds from this to another index
+    let paceHh = (paceSec/60)/60;
+    console.log(paceHh);
+    // convert the remains to minutes and place the result in another index
+    /*
+    let paceMm = (paceHh-)
+    console.log(paceHh);
+    // convert the remains to seconds and place the result in the last index
+
+
+    // convert time pr. km to hh:mm:ss
+    let pace = arr[
+        
+        paceHh,
+     
+        {"id": h, 
+        "time": (paceSec/60)/60)},
+        {"id": m,
+        "time": ()}
+        */
+ 
+
+    // create array of hh:mm:ss
+   
     
 /*
     let arr = s.split(':');
@@ -21,7 +50,7 @@ const convert = function (e) {
     let m = arr[1];
     let s = arr[2];
 */  
-    $('hours').innerHTML = pace;
+    $('hours').innerHTML = paceSec;
     /*$('minutes').innerHTML = m;
     $('seconds').innerHTML = s;
     */
