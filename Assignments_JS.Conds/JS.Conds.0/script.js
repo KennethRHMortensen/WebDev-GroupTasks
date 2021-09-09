0e5df2902d7cfef1bc5e56eec4745fa54da915b8
Kenneth <kenneth@kennt.dk>
2021-09-09 17:53:38 +0200

    organised JS and added comments

--- Assignments_JS.Conds/JS.Conds.0/script.js
+++ Assignments_JS.Conds/JS.Conds.0/script.js
@@ -1,39 +1,46 @@
 'use strict'
+// shortkey for document.getElementById()
 const $ = function (foo) {
     return document.getElementById(foo);
 };
 
-let input = $('number');
-let amount = $('rollAmount');
-let one = $('one');
-let oneVal = 0;
-let two = $('two');
-let twoVal = 0;
-let three = $('three');
-let threeVal = 0;
-let four = $('four');
-let fourVal = 0;
-let five = $('five');
-let fiveVal = 0;
-let six = $('six');
-let sixVal = 0;
+// Assign variable alias
+let input   = $('number'); 
+let amount  = $('rollAmount');
 
+// sides of the dice
+let one     = $('one');
+let two     = $('two');
+let three   = $('three');
+let four    = $('four');
+let five    = $('five');
+let six     = $('six');
 
+// default values before rolling the die
+let oneVal      = 0;
+let twoVal      = 0;
+let threeVal    = 0;
+let fourVal     = 0;
+let fiveVal     = 0;
+let sixVal      = 0;
 
+// function that rolls the dice 
 function rollDie(e){
     e.preventDefault();
-    input = $('number').value -0;
-    result();
-    console.log(input);
-    amount.innerHTML = input;
+    input = parseInt(input.value);      // re-assign input value 
+    amount.innerHTML = input;           // print input value in HTML
+    console.log('The input value was ' + input + ' rolls');
+    result();                           // run result function 
 }
 
-function result(e){
-    for (let i = 0; i < input; i++) {
-        let roll = Math.floor(Math.random() * 6) +1;
+// function that increments and prints results of rolls
+function result(){
+    for (let i = 0; i < input; i++) {                   // loop until input i = input value
+        let roll = Math.floor(Math.random() * 6) +1;    // assign variable with a random number between 1 and 6
+        console.log('this roll hit ', roll);            // console log each roll
         if(roll === 1){
-            oneVal++;
-            one.innerHTML = oneVal
+            oneVal++;                                   // increment variable
+            one.innerHTML = oneVal                      // print incremented value in HTML
         }
         else if(roll === 2){
             twoVal++;
@@ -59,6 +66,5 @@ function result(e){
     }
 }
 
-
-
+// On submit of formular, run function rollDie
 $('rollDice').addEventListener('submit',rollDie);
